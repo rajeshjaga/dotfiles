268212c65231cf7126b980e8644f61a5658aaa4c
rajeshjaga <rajaga223@gmail.com>
2024-07-15 22:34:41 +0530

    lolcal

--- .config/scripts/kitty.sh
+++ .config/scripts/kitty.sh
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-#/bin/bash
-
-bash -c "LIBGL_ALWAYS_SOFTWARE=1 alacritty"
-=======
 #!/bin/bash
 bash -c "LIBGL_ALWAYS_SOFTWARE=1 kitty $1"
->>>>>>> 9954f3f (commit: hop this push works)
